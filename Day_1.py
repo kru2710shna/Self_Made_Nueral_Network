8ab5969103cec60b0db98c997b3bda52ffb28435
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-10-31 13:01:13 -0700

    Forward Propogation

--- Day_1.py
+++ Day_1.py
@@ -1,23 +1,26 @@
 import numpy as np
 
-inputs = [[1, 2, 3, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]]
+X = [[1, 2, 3, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]]
 
+# Hidden Layers
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-weights = [[0.2, 0.8, -0.5, 1.0], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
-
-
-weights2 = [[0.1, -0.14, 0.5], [0.5, 0.12, 0.33], [0.44,0.73,-0.13]]
-
-
-biases = [2.0, 3.0, 0.5]
-
-biases2 = [-1,2,-0.5]
-
-layer1_output = np.dot(inputs, np.array(weights).T) + biases
-layer2_output = np.dot(layer1_output, np.array(weights2).T) + biases2
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
 if __name__ == "__main__":
-    print(layer1_output)
-    print("Layer_1 output")
-    print(layer2_output)
-    print("Layer_2 output")
+    layer1 = Layer_Dense(4, 5)
+    layer2 = Layer_Dense(5, 2)
+    
+    print("Layer 1")
+    layer1.forward(X)
+    print("Layer1 output shape:", layer1.output.shape)
+    print(layer1.output)
+    
+    print("Layer 2")
+    layer2.forward(layer1.output)
+    print("Layer2 output shape:", layer2.output.shape)
+    print(layer2.output)
