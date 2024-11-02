ef6b3372ae31718df5703bf6de762db7744646c4
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-11-01 18:29:23 -0700

    Activation RELU Function

--- Day_1.py
+++ Day_1.py
@@ -1,6 +1,16 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from nnfs.datasets import spiral_data
+np.random.seed(0)
+# X = [[1, 2, 3, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]]
 
-X = [[1, 2, 3, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]]
+X, y = spiral_data(100, 3)
+
+# Threshold example
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+outputs = []
+for i in inputs:
+    outputs.append(i if i > 0 else 0)
 
 # Hidden Layers
 class Layer_Dense:
@@ -11,16 +21,19 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
+# RELU Function
+class Activation_Function:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
 if __name__ == "__main__":
-    layer1 = Layer_Dense(4, 5)
-    layer2 = Layer_Dense(5, 2)
+    layer1 = Layer_Dense(2, 5)
+    activation1 = Activation_Function()
     
-    print("Layer 1")
+    # Forward pass through first layer
     layer1.forward(X)
-    print("Layer1 output shape:", layer1.output.shape)
-    print(layer1.output)
+    # Applying activation function
+    activation1.forward(layer1.output)
     
-    print("Layer 2")
-    layer2.forward(layer1.output)
-    print("Layer2 output shape:", layer2.output.shape)
-    print(layer2.output)
+    # Print the output of the activation function
+    print("Activation output:\n", activation1.output)
